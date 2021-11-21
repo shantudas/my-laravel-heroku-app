d779c842d00273c200e005fbbac62738b4449948
shantudashbd <shantudasbd@yahoo.com>
2021-11-21 17:21:23 +0600

    create at added in coordinates

--- resources/views/coordinate.blade.php
+++ resources/views/coordinate.blade.php
@@ -12,19 +12,20 @@
             <th scope="col">speed</th>
             <th scope="col">accuracy</th>
             <th scope="col">time stamps</th>
+            <th scope="col">Created at</th>
         </tr>
     </thead>
     <tbody>
-    @foreach ($userCoordinates as $coordinate)
+        @foreach ($userCoordinates as $coordinate)
         <tr>
-           
+
             <td>{{$coordinate->id}}</td>
             <td>{{$coordinate->latitude}}</td>
             <td>{{$coordinate->longitude}}</td>
             <td>{{$coordinate->speed}}</td>
             <td>{{$coordinate->accuracy}}</td>
             <td>{{ Carbon\Carbon::createFromTimestamp($coordinate->time_stamps)->toDateTimeString() }}</td>
-          
+            <td>{{$coordinate->created_at}}</td>
         </tr>
         @endforeach
     </tbody>
